12724886ee20088d58f7ab699031e33251210a27
MudityaQQ <147902558+MudityaQQ@users.noreply.github.com>
2023-11-06 10:23:22 +0530

    first commit

---
+++ server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+
+io.on('connection', (socket) => {
+    socket.on('offer', (data) => {
+        socket.broadcast.emit('offer', data);
+    });
+
+    socket.on('answer', (data) => {
+        socket.broadcast.emit('answer', data);
+    });
+
+    socket.on('ice-candidate', (data) => {
+        socket.broadcast.emit('ice-candidate', data);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
